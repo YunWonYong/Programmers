a217f527a4701f34330b3cbdabeabc8de9bc1504
ywyi1992 <ywyi1992@gmail.com>
2021-10-11 20:29:11 +0900

    test

--- Algorithms/src/ywy/level1/RecommendNewId.java
+++ Algorithms/src/ywy/level1/RecommendNewId.java
@@ -17,7 +17,7 @@ public class RecommendNewId {
     	return backDotRemove(str.replaceAll("^[\\.]", ""));
     }
     
-    private String backDotRemove(String str) {
+    private String backDotRemove(Strin  g str) {
     	return str.replaceAll("[\\.]$", "");
     }
     
