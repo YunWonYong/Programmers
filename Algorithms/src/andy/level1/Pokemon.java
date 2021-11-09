a142a5b9b8eebb64a902aca951bf2c3edcb1eac8
andy <scym14@gmail.com>
2021-11-10 00:18:42 +0900

    프로그래머스 Level 1,폰켓몬

---
+++ Algorithms/src/andy/level1/Pokemon.java
@@ -0,0 +1,17 @@
+package andy.level1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//프로그래머스 Level 1,폰켓몬
+public class Pokemon {
+    public int solution(int[] nums) {
+        int answer = 0;      
+        int cnt = nums.length/2;
+        Set<Integer> set = new HashSet<>();
+        for(int d:nums) set.add(d);
+        if(cnt<set.size()) answer = cnt;
+        else answer = set.size();
+        return answer;
+    }
+}
