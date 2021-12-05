7a8641bbbe79740ea6cc318b492455e7b8ea2d2a
ywyi1992 <ywyi1992@gmail.com>
2021-12-05 09:11:52 +0900

    2021-12-05 #101 Clear

---
+++ Algorithms/src/ywy/java/level1/test/FirstWeekOfDecember.java
@@ -0,0 +1,11 @@
+package ywy.java.level1.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SecretMapTest.class })
+public class FirstWeekOfDecember {
+
+}
