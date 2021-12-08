67eb90c433d23ea65157e92296231577542a77dc
PARK JOON GYU <vpark45@gmail.com>
2021-12-08 21:08:18 +0900

    Add Center.java

---
+++ Algorithms/src/nattybear/level1/Center.java
@@ -0,0 +1,18 @@
+class Solution {
+  public String solution(String s) {
+    int len = s.length();
+    int i = (int) Math.floor(len / 2);
+    if (len % 2 == 0)
+      return s.substring(i-1, i+1);
+    else
+      return s.substring(i, i+1);
+  }
+}
+
+public class Center {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.solution("abcde"));
+    System.out.println(s.solution("qwer"));
+  }
+}
