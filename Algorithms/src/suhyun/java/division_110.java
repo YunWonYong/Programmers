4bedd6ffcaed917b27bc1bc45eba2340e83e6755
suhyun choi <95855890+suhyun96@users.noreply.github.com>
2021-12-14 22:01:29 +0900

    Modify the class name and file name
    클래스명과 파일명 변경

--- Algorithms/src/suhyun/java/division_110.java
+++ Algorithms/src/suhyun/java/division_110.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
-class Solution {
-    public int[] solution(int[] arr, int divisor) {
+class division_110 {
+    public int[] division_110(int[] arr, int divisor) {
                 
         int[] answer = {};
         int count = 0; // 나누어진 수 만큼 answer 초기화
