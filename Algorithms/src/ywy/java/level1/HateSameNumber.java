00f168f4e541d62efd6b2dcb2c89ecd0ad37565d
ywyi1992 <ywyi1992@gmail.com>
2021-12-14 20:16:49 +0900

    2021-12-14 #109 clear

---
+++ Algorithms/src/ywy/java/level1/HateSameNumber.java
@@ -0,0 +1,23 @@
+package ywy.java.level1;
+
+public class HateSameNumber {
+	public HateSameNumber() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int[] solution(int[] arr) {
+        int range = arr.length;
+        int index = 0;
+        int element = arr[index];
+        int addIndex = 1;
+        while(++index < range) {
+            element = arr[index];
+            if (arr[addIndex - 1] != element) {
+                arr[addIndex++] = element;
+            }
+        }
+        int[] answer = new int[addIndex];
+        System.arraycopy(arr, 0, answer, 0, addIndex);
+        return answer;
+    }
+}
