37598bb8e1be07b934fdde2e533c246c877f5f29
ywyi1992 <ywyi1992@gmail.com>
2021-12-15 20:38:53 +0900

    2021-12-15 Project Clean

--- Algorithms/src/andy/javascript/level1/Pokemon.js
+++
@@ -1,9 +0,0 @@
-// 프로그래머스 Level 1,폰켓몬
-function solution(nums) {
-    var answer = 0;
-    var cnt = parseInt(nums.length/2);
-    var set = new Set(nums);
-    if(cnt<set.size) answer=cnt;
-    else answer = set.size;
-    return answer;
-}
--- Algorithms/src/nattybear/level1/SumTwo.js
+++
@@ -1,7 +0,0 @@
-function solution(a, b) {
-  const n = Math.abs(a - b);
-  return n * (a + b) / 2;
-}
-
-console.log(solution(3, 5));
-console.log(solution(5, 3));
