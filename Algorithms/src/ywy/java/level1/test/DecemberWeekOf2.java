04aaceff3d2e11bd40fddf39bec69c01ec27e2bd
ywyi1992 <ywyi1992@gmail.com>
2021-12-15 20:38:53 +0900

    2021-12-15 Project Clean

--- Algorithms/src/ywy/java/level1/test/DecemberWeekOf2.java
+++ Algorithms/src/ywy/java/level1/test/DecemberWeekOf2.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ywy.java.level1.test.cases.DivisibleNumberTest;
 import ywy.java.level1.test.cases.HateSameNumberTest;
+import ywy.java.level1.test.cases.SumBetweenTwoNumbersTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ HateSameNumberTest.class  })
+@SuiteClasses({ HateSameNumberTest.class, DivisibleNumberTest.class, SumBetweenTwoNumbersTest.class})
 public class DecemberWeekOf2 {
 	@AfterClass
 	public static void clear() {
