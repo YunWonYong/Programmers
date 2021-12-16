5e644da53480cf4856c715078231784f48e90b4f
Lee-jh01 <wjdgmllee0423@naver.com>
2021-12-16 19:47:15 +0900

    코드수정

--- Algorithms/src/ljh/level1/js/DivideArrayNumber.js
+++ Algorithms/src/ljh/level1/js/DivideArrayNumber.js
@@ -1,15 +1,14 @@
 let arr = [15,5,9,7,10];
 let divider = 5;
-solution(arr, divider);
-
-arr = arr.filter((value) => value % divider ===0);
-if(arr.length < 1){
-    arr[0] = -1;
-}else{
-    arr.sort((a, b) => a-b);
-}
+arr = solution(arr, divider);
 
 console.log(arr);
 function solution(arr, divisor) {
+	arr = arr.filter((value) => value % divider ===0);
+	if(arr.length < 1){
+	    arr[0] = -1;
+	}else{
+	    arr = arr.sort((a, b) => a-b);
+	}
     return arr;
 }
