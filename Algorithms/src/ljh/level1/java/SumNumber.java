9c02a541b0cd24c7f724c7e7ecef081347150d9c
Lee-jh01 <wjdgmllee0423@naver.com>
2021-12-16 17:37:59 +0900

    leve1. 두 정수 사이의 합

---
+++ Algorithms/src/ljh/level1/java/SumNumber.java
@@ -0,0 +1,23 @@
+package ljh.level1.java;
+
+/*
+ * 프로그래머스 - 두 정수 사이의 합
+ */
+class SumNumber {
+    public long solution(int a, int b) {
+        long answer = 0;
+        long max = b;
+        long min = a;
+
+        if(a > b){
+            max = a;
+            min = b;
+        }
+
+        for(long i=min; i<=max; i++){
+            answer += i;
+        }
+
+        return answer;
+    }
+}
---
+++ Algorithms/src/ljh/level1/js/SumNumber.js
@@ -0,0 +1,9 @@
+a = 3;
+b = 5;
+let answer = solution(a, b);
+console.log(answer);
+
+function solution(a,b){
+    var answer = 0;
+    return answer;
+}
