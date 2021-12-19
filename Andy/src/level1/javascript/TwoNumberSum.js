c8a789b6e2a7c5f386c224742e6076a81e406b45
ywyi1992 <ywyi1992@naver.com>
2021-12-19 11:50:34 +0900

    2021-12-19 repository renewal into ywy

--- Andy/src/level1/javascript/TwoNumberSum.js
+++
@@ -1,4 +0,0 @@
-//프로그래머스 Level 1,두 정수 사이의 합
-function solution(a, b) {
-    return (a+b)*(Math.abs(a-b)+1)/2;
-}
--- Andy/src/level1/javascript/year2016.js
+++
@@ -1,4 +0,0 @@
-//프로그래머스 Level 1, 2016년
-function solution(a, b) {
-    return (new Date(2016,a-1,b)).toString().substring(0,3).toUpperCase();
-}
