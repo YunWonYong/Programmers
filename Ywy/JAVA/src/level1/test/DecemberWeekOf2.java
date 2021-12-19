4d583887223bfcc45509343bcf512f273dec427c
ywyi1992 <ywyi1992@naver.com>
2021-12-19 12:28:39 +0900

    2021-12-20 repository renewal into ywy 1. add .metadatas to ignore 2. unitTest add

--- Ywy/JAVA/src/level1/test/DecemberWeekOf2.java
+++ Ywy/JAVA/src/level1/test/DecemberWeekOf2.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import level1.test.cases.DivisibleNumberTest;
 import level1.test.cases.HateSameNumberTest;
+import level1.test.cases.TwoNumberPlusTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ HateSameNumberTest.class  })
+@SuiteClasses({ HateSameNumberTest.class, DivisibleNumberTest.class, TwoNumberPlusTest.class })
 public class DecemberWeekOf2 {
 	@AfterClass
 	public static void clear() {
