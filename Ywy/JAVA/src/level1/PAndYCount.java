d9af0ab4728b529271687ad6b982e41ab866ac0c
ywyi1992 <ywyi1992@naver.com>
2021-12-20 23:55:34 +0900

    master branch .gitignore

--- Ywy/JAVA/src/level1/PAndYCount.java
+++
@@ -1,9 +0,0 @@
-package level1;
-
-public class PAndYCount {
-	public boolean solution(final String s) {
-		int pCount = s.replaceAll("[^pP]", "").length();
-		int yCount = s.replaceAll("[^yY]", "").length();
-		return pCount == yCount;
-	}
-}
