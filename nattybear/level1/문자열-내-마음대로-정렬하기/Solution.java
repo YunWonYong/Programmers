2f6b8a7b4ef0e6f2ddda2f3e609fd4b2f4c373df
PARK JOON GYU <vpark45@gmail.com>
2021-12-21 13:46:11 +0900

    Fix bug
    n번째 문자가 같을 경우 원래 문자열로 정렬하는 코드가 없었음

--- nattybear/level1/문자열-내-마음대로-정렬하기/Solution.java
+++ nattybear/level1/문자열-내-마음대로-정렬하기/Solution.java
@@ -9,7 +9,11 @@ class Solution {
         public int compare(String s1, String s2) {
           char c1 = s1.charAt(n);
           char c2 = s2.charAt(n);
-          return Character.compare(c1, c2);
+          int result = c1 - c2;
+          if (result != 0)
+            return result;
+          else
+            return s1.compareTo(s2);
         }
       })
       .toArray(String[]::new);
