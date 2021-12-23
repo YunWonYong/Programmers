95d942f0f697895bfe253f477fea6505b8f3e22c
Lee-jh01 <wjdgmllee0423@naver.com>
2021-12-23 17:47:01 +0900

    문자열 내림차순으로 정렬

--- ljh/java/src/level1/SortInDescendingOrder.java
+++ ljh/java/src/level1/SortInDescendingOrder.java
@@ -1,12 +1,25 @@
 package level1;
 
+import java.util.Arrays;
+
 /*
  * 문자열 내림차순으로 정렬하기
  */
 
 public class SortInDescendingOrder {
 	public String solution(String s) {
-        String answer = "";
-        return answer;
-    }
+		int[] nums = new int[s.length()];
+		String answer = "";
+		
+		for(int i=0; i<s.length(); i++) {
+			nums[i] = -1 * s.charAt(i);
+		}
+		Arrays.sort(nums);
+		for(int j=0; j<nums.length; j++) {
+			 answer += (char)(nums[j] * -1);
+		}
+		
+		return answer;
+	}
+	
 }
--- ljh/javascript/level1/SortInDescendingOrder.js
+++ ljh/javascript/level1/SortInDescendingOrder.js
@@ -1,7 +1,9 @@
 // 문자열 내림차순으로 정렬하기(12/18 - 12/24)
 export default class SortInDescendingOrder {
     solution = (s) => {
-        var answer = '';
-        return answer;
+        let answer = [];
+        for(let c of s) answer.push(c.charCodeAt());
+        answer.sort((a, b) => b - a);
+        return answer.map((value) => String.fromCharCode(value)).join('');
     }
 }
