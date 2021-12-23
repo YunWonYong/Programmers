be893455c908e04176bd1cce77aaf9a15ae998e8
suhyun choi <95855890+suhyun96@users.noreply.github.com>
2021-12-23 22:54:50 +0900

    Update PtoY.java

--- Suhyun/Java/PtoY.java
+++ Suhyun/Java/PtoY.java
@@ -1,29 +1,31 @@
 public class PtoY {
     public static void main(String[] args) {
-        String s = "pPoooyY";
+        boolean answer =true;
         char[] arr = s.toCharArray();
         int y_count = 0;
         int p_count = 0;
-        boolean answer = true;
 
         for (int i = 0; i < s.length(); i++) {
-            
-            if (arr[i] == 80 || arr[i] == 112) {
-                p_count++;
-            }
-            
-            else if (arr[i] == 89 || arr[i] == 121) {
-                y_count++;
+
+            switch (arr[i]) {
+                case 89:
+                case 121:
+                    y_count++;
+                    break;
+                case 80:
+                case 112:
+                    p_count++;
+                    break;
             }
         }
-        if (y_count == 0 && p_count == 0) {
+        if (y_count == 0 && p_count == 0||y_count == p_count) {
             //return answer;
-        } else if (y_count == p_count) {
-           // return answer;
-        } else {
+            System.out.println(answer);
+       } else {
             answer = false;
             //return answer;
-        }
-
+            System.out.println(answer);
+       }
+        
     }
 }
