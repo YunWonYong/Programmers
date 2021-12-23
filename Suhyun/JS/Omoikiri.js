cc8dd111ad67425598198342059dea93891eb0f5
suhyun choi <95855890+suhyun96@users.noreply.github.com>
2021-12-23 20:58:24 +0900

    Create Omoikiri.js
    2021-12-23
    문자열 내 마음대로 정렬하기

---
+++ Suhyun/JS/Omoikiri.js
@@ -0,0 +1,13 @@
+    let answer=[];
+    for(let i = 0 ; i<strings.length ; i++)
+    {
+        answer.push(strings[i].charAt(n)+strings[i]);
+    }
+    answer.sort();
+    
+    for(let i = 0 ; i<strings.length ; i++)
+    {
+        answer[i]=answer[i].substr(1,answer[i].length)
+    }
+    
+    console.log(answer);
