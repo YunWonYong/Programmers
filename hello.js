62cf9c685824c111066e707efef367986737b3c6
준규 <vpark45@gmail.com>
2021-12-26 00:28:13 +0900

    Add api to make label

--- hello.js
+++ hello.js
@@ -1,1 +1,54 @@
-console.log("hello");
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const json = ''
+
+readline.question('', s => {
+  json = JSON.parse(s)
+  readline.close()
+})
+
+console.log(json);
+
+const https = require('https')
+
+const data = JSON.stringify({
+  labels: ["test"]
+})
+
+const username = 'vpark45@gmail.com'
+
+const password = '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'
+
+const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64')
+
+const options = {
+  hostname: 'api.github.com',
+  port: 443,
+  path: json['pull_request']['_links']['html'] + '/labels',
+  method: 'POST',
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'User-Agent': 'vpark45@gmail.com',
+    'Authorization': auth,
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+
+req.end()
