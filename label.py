32051193ac7eee0dcc49665d6f54c0b09b9ff70a
준규 <vpark45@gmail.com>
2021-12-26 19:34:57 +0900

    Delete hello.js and modify label.py

--- label.py
+++ label.py
@@ -1,34 +1,8 @@
-import base64
-import json
 import sys
-import urllib.request
 
 filename = sys.argv[1]
 with open(filename) as f:
-  buf = f.read()
-dic = json.loads(buf)
-url = dic['pull_request']['_links']['issue']['href'] + '/labels'
+  b = f.read().decode()
+j = json.loads(b)
+url = j['pull_request']['_links']['issue']['href'] + '/labels'
 print(url)
-
-username = 'vpark45@gmail.com'
-password = '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'
-toBeEncoded = username + ':' + password
-auth = 'Basic ' + base64.b64encode(toBeEncoded.encode()).decode('ascii')
-print(auth)
-
-data = json.dumps({'labels': ['test']}).encode()
-
-req = urllib.request.Request(url)
-req.add_header('Accept', 'application/vnd.github.v3+json')
-req.add_header('Authorization', auth)
-req.add_header('Content-Length', len(data))
-req.add_header('Content-Type', 'application/json')
-req.add_header('User-Agent', 'vpark45@gmail.com')
-
-try:
-  f = urllib.request.urlopen(req, data)
-  print(f.read().decode())
-except urllib.error.HTTPError as e:
-  print(e.reason)
-
-f.close()
