1b0e08fa59321a0d2559b462bd32cd4ab9b36307
준규 <vpark45@gmail.com>
2021-12-26 21:47:27 +0900

    Modify password

--- label.py
+++ label.py
@@ -1,9 +1,25 @@
+import base64
 import json
 import sys
+import urllib.request
 
 filename = sys.argv[1]
 with open(filename) as f:
   b = f.read()
-j = json.loads(b)
-url = j['pull_request']['_links']['issue']['href'] + '/labels'
-print(url)
+d = json.loads(b)
+url = d['pull_request']['_links']['issue']['href'] + '/labels'
+
+username = 'vpark45@gmail.com'
+password = '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'
+auth = 'Basic ' + base64.b64encode((username + ':' + password).encode()).decode()
+url = 'https://api.github.com/repos/nattybear/Programmers/issues/23/labels'
+data = json.dumps({'labels' : ['test']}).encode()
+
+req = urllib.request.Request(url, data)
+req.add_header('Accept', 'application/vnd.github.v3+json')
+req.add_header('Authorization', auth)
+
+with urllib.request.urlopen(req) as f:
+  b = f.read()
+
+print(b)
