4dc09d37709c4fb787792b49e12bc9e74ea31006
준규 <vpark45@gmail.com>
2021-12-26 22:13:39 +0900

    Separate personal access tokens

--- label.py
+++ label.py
@@ -3,16 +3,18 @@ import json
 import sys
 import urllib.request
 
+'''
 filename = sys.argv[1]
 with open(filename) as f:
   b = f.read()
 d = json.loads(b)
 url = d['pull_request']['_links']['issue']['href'] + '/labels'
+'''
 
 username = 'vpark45@gmail.com'
-password = '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'
+password = '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'
 auth = 'Basic ' + base64.b64encode((username + ':' + password).encode()).decode()
-# url = 'https://api.github.com/repos/nattybear/Programmers/issues/23/labels'
+url = 'https://api.github.com/repos/nattybear/Programmers/issues/23/labels'
 data = json.dumps({'labels' : ['test']}).encode()
 
 req = urllib.request.Request(url, data)
