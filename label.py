7335d71307b8c50ba5baca2b131aa34f14fe8a37
준규 <vpark45@gmail.com>
2021-12-26 19:37:26 +0900

    Delete method decode()

--- label.py
+++ label.py
@@ -2,7 +2,7 @@ import sys
 
 filename = sys.argv[1]
 with open(filename) as f:
-  b = f.read().decode()
+  b = f.read()
 j = json.loads(b)
 url = j['pull_request']['_links']['issue']['href'] + '/labels'
 print(url)
