cba67bc8c129198948d103f4818ca0142ef52e06
준규 <vpark45@gmail.com>
2021-12-27 16:18:14 +0900

    Separate add label logic and make it function

--- .github/workflows/add-approve.py
+++ .github/workflows/add-approve.py
@@ -1,28 +1,3 @@
-import base64
-import configparser
-import json
-import sys
-import urllib.request
+import label
 
-filename = sys.argv[1]
-with open(filename) as f:
-  b = f.read()
-d = json.loads(b)
-url = d['pull_request']['_links']['issue']['href'] + '/labels'
-
-config = configparser.ConfigParser()
-config.read('.label.ini')
-default = config['DEFAULT']
-username = default['username']
-password = default['password']
-auth = 'Basic ' + base64.b64encode((username + ':' + password).encode()).decode()
-data = json.dumps({'labels' : ['approve']}).encode()
-
-req = urllib.request.Request(url, data)
-req.add_header('Accept', 'application/vnd.github.v3+json')
-req.add_header('Authorization', auth)
-
-with urllib.request.urlopen(req) as f:
-  b = f.read()
-
-print(b)
+label.addLabels(['approve'])
