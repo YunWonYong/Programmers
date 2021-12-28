8719c702d91d95a4e5b11be4dcc05fdca6acb2be
준규 <vpark45@gmail.com>
2021-12-28 20:52:49 +0900

    Delete function even

--- nattybear/level1/소수-찾기/solution.js
+++ nattybear/level1/소수-찾기/solution.js
@@ -6,16 +6,12 @@ function solution(n) {
   return count
 }
 
-function even(n) {
-  return n % 2 == 0
-}
-
 function prime(n) {
   if (n < 2)
     return false
   if (n == 2)
     return true
-  if (even(n))
+  if (n % 2 == 0)
     return false
   for (let i = 3; i <= Math.sqrt(n); i += 2)
     if (n % i == 0)
