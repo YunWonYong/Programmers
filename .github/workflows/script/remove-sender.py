1979680b1725c6af22e2d80c1cd2eb3984ad25bc
준규 <vpark45@gmail.com>
2021-12-28 15:40:56 +0900

    Add test for removing approve label

--- .github/workflows/script/remove-sender.py
+++ .github/workflows/script/remove-sender.py
@@ -2,4 +2,10 @@ import label
 
 sender = 'branch ' + label.jsonData['sender']['login']
 label.removeLabel(sender)
-print(label.jsonData['labels'])
+
+labels = label.jsonData['pull_request']['labels']
+print(labels)
+print("number of labels : " + str(len(labels)))
+if len(labels) == 1 and labels[0]['name'] == 'approve':
+  label.removeLabel('approve')
+  label.addLabel('merge')
