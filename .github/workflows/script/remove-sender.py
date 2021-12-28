d20595d06640e3cbd505b23c6d4297d5fb6ce62a
준규 <vpark45@gmail.com>
2021-12-28 15:44:42 +0900

    Remove test code

--- .github/workflows/script/remove-sender.py
+++ .github/workflows/script/remove-sender.py
@@ -4,8 +4,6 @@ sender = 'branch ' + label.jsonData['sender']['login']
 label.removeLabel(sender)
 
 labels = label.jsonData['pull_request']['labels']
-print(labels)
-print("number of labels : " + str(len(labels)))
 if len(labels) == 1 and labels[0]['name'] == 'approve':
   label.removeLabel('approve')
   label.addLabel('merge')
