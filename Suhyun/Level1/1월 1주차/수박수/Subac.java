2c80c2922fffefb214e340973ae73c1537e126e4
suhyun96 <jhk8500@gmail.com>
2022-01-01 01:49:29 +0900

    2022-01-01 수박수 테스트2 정수 바꾸기

--- Suhyun/Level1/1월 1주차/수박수/Subac.java
+++ Suhyun/Level1/1월 1주차/수박수/Subac.java
@@ -2,17 +2,23 @@ public class Subac {
     public static void main(String[] args) {
         int n=3;
         String answer="";
-        int count=1;
-
+        
+        //6.13
         for(int i=0; i<n; i++){
-            if(count%2!=0){
-                answer=answer+"수";
-            }
-            else{
-                answer=answer+"박";
-            }
-            count++;
+            answer+= i%2==0 ? "수" : "박";
         }
+
+        // 6.84
+        // int count=1;
+        // for(int i=0; i<n; i++){
+        //     if(count%2!=0){
+        //         answer=answer+"수";
+        //     }
+        //     else{
+        //         answer=answer+"박";
+        //     }
+        //     count++;
+        // }
 
         System.out.print(answer);
         //return asnwer;
