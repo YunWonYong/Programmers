64fdac9eb827b0912a4f885947f3d792a22e7711
Lee-jh01 <wjdgmllee0423@naver.com>
2022-01-05 15:56:04 +0900

    220105 문자열을 정수로 바꾸기

---
+++ ljh/javascript/level1/StringToInteger.js
@@ -0,0 +1,9 @@
+// 문자열을 정수로 바꾸기 (211231 - 220107)
+export default class StringToInteger{
+    solution = (s) => {
+        return parseInt(s);
+    }
+    solution2 = (s) => {
+        return Number(s);
+    }
+}
