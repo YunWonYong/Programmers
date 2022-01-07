ce70b32e72f083d2f2b8b3b90f786b9439755bf3
Lee-jh01 <wjdgmllee0423@naver.com>
2022-01-07 14:10:48 +0900

    220107 문제 해결

--- ljh/java/src/level1/CaesarCipher.java
+++ ljh/java/src/level1/CaesarCipher.java
@@ -5,38 +5,32 @@ package level1;
  */
 
 // 알파벳 소문자, 대문자 분리 후 문제 수정. 9,11,12,13 실패
+// 조건식 간단하게 다듬기
+// 문제 이해를 잘못해서 연속된 여러칸의 공백을 한칸 공백으로 줄이기를 함... => 공백 줄이기 적용 X
 
 public class CaesarCipher {
 
 	public String solution(String s, int n) {
 		StringBuffer sb = new StringBuffer(s.length());
-		boolean space = false; // 이전 문자가 공백인지 아닌지 판별
 		char c; // 문자 임시 저장
 		
 		for(int i=0; i<s.length(); i++) {			
 			c = s.charAt(i);
 			
 			if(c == ' ') { // 문자가 공백일 때
-				if(space == true) {
-					continue;
-				}
 				sb.append(' ');
-				space = true;
-				
 			}else if('a' <= c && c <= 'z'){ //알파벳 소문자일 때
 				c = (char)(c + n);
 				if(c > 'z') {
 					c = (char)(c - 26);
 				}
 				sb.append(c);
-				space = false;
 			}else { //알파벳 대문자일 때
 				c = (char)(c + n);
 				if(c > 'Z') {
 					c = (char)(c - 26);
 				}
 				sb.append(c);
-				space = false;
 			}
 		}
 		return sb.toString();
--- ljh/javascript/level1/CaesarCipher.js
+++ ljh/javascript/level1/CaesarCipher.js
@@ -2,12 +2,7 @@
 export default class CaesarCipher {
     
     solution = (s , n) => {
-        //const upper_pattern = /^6{1}[5-9]{1}$|^[7-8]{1}[0-9]{1}$|^9{1}1{1}$/g; //65-90, 대문자
-        //const lower_pattern = /^9{1}[7-9]{1}$|^1{1}[0,1]{1}[0-9]{1}$|^1{1}2{1}[0-2]{1}$/g; //97-122, 소문자
-
-        let replaced_str = s.replace(/ +/g,' ');
-
-        replaced_str = replaced_str.replace(/[a-z]/g, (value) => {
+        let replaced_str = s.replace(/[a-z]/g, (value) => {
             value = value.charCodeAt() + n;
             return value > 122 ? String.fromCharCode(value - 26) : String.fromCharCode(value); 
         });
