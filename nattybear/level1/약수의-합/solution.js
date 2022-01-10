9fdb0217c2eb7af917427805b8731cfaf451cbdd
준규 <vpark45@gmail.com>
2022-01-10 16:56:00 +0900

    Add test case when n is 0 or 1

--- nattybear/level1/약수의-합/solution.js
+++ nattybear/level1/약수의-합/solution.js
@@ -1,12 +1,17 @@
 function solution(n) {
+  if (n == 0 || n == 1)
+    return n
+
   const sum = divisor(n).reduce((acc, x) => acc + x)
   return sum
 }
 
 function divisor(n) {
   const ds = [1, n]
+
   for (let i = 2; i < n; i++)
     if (n % i == 0)
       ds.push(i)
+
   return ds
 }
