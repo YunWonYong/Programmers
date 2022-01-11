55a28bd63385087d15c43249e3bd605728cc218d
준규 <vpark45@gmail.com>
2022-01-11 19:08:21 +0900

    Fix and solve lesson using C language

--- nattybear/level1/약수의-합/solution.c
+++ nattybear/level1/약수의-합/solution.c
@@ -2,9 +2,9 @@ int solution(int n) {
   if (n == 0 || n == 1)
     return n;
 
-  int sum = 0;
+  int sum = 1 + n;
 
-  for (int i = 2; i <= n; i++)
+  for (int i = 2; i < n; i++)
     if (n % i == 0)
       sum += i;
 
