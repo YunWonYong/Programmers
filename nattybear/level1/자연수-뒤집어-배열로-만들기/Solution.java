2ced3d66273db8676386ad3d30b0d2ac605f58c5
준규 <vpark45@gmail.com>
2022-01-13 13:11:36 +0900

    Fix bug

--- nattybear/level1/자연수-뒤집어-배열로-만들기/Solution.java
+++ nattybear/level1/자연수-뒤집어-배열로-만들기/Solution.java
@@ -1,10 +1,17 @@
 class Solution {
   public int[] solution(long n) {
-    return String.valueOf(n)
+    int[] arr = String.valueOf(n)
       .chars()
-      .map(x -> Integer.parseInt(String.valueOf((char) x)) * -1)
-      .sorted()
-      .map(x -> x * -1)
+      .map(x -> Integer.parseInt(String.valueOf((char) x)))
       .toArray();
+    return reverse(arr);
+  }
+
+  public int[] reverse(int[] arr) {
+    int n = arr.length;
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+      result[n - i - 1] = arr[i];
+    return result;
   }
 }
