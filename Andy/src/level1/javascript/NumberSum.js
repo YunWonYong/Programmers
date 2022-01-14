128fbc790f21e8eb8c9f1332b3d8d7ca4e26883d
andy <scym14@gmail.com>
2022-01-14 21:58:29 +0900

    프로그래머스 Level 1,자릿수 더하기

--- Andy/src/level1/javascript/NumberSum.js
+++ Andy/src/level1/javascript/NumberSum.js
@@ -1,7 +1,4 @@
 //프로그래머스 Level 1,자릿수 더하기 
 function solution(n) {
-    let answer = n;
-    for(let i=1;i<n;i++)
-        if(n%i == 0) answer += i;
-    return answer;
+	return [...(""+n)].map(Number).reduce( (a,d) => a+d);
 }
