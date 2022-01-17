ecf57bd7d64343ba3796731c545190e64b772863
andy <scym14@gmail.com>
2022-01-17 21:00:23 +0900

    프로그래머스 Level 1,짝수와 홀수

--- Andy/src/level1/java/DivideNumberArray.java
+++ Andy/src/level1/java/DivideNumberArray.java
@@ -7,18 +7,15 @@ import java.util.List;
 
 //프로그래머스 Level 1,나누어 떨어지는 숫자 배열
 public class DivideNumberArray {
-    public int[] solution(int[] arr, int divisor) {
+    public int[] solution(int[] arr) {
         int[] answer = {-1};
-        List<Integer> list = new ArrayList<Integer>();
-        for(int d:arr) {
-            if(d%divisor == 0) {
-                list.add(d);
-            }                
-        }        
-        
-        if(list.size() == 0) return answer;
-        answer = list.stream().mapToInt(Integer::intValue).toArray();
-        Arrays.sort(answer);
+    	if(arr.length <= 1) return answer;
+
+    	List<Integer> alist = new ArrayList<Integer>();
+    	int min = arr[0];
+    	for(int d:arr) if(min>d) min = d;
+    	for(int d:arr) if(d != min) alist.add(d);
+        answer =  alist.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
