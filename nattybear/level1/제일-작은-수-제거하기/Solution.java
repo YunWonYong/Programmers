296e4f7097ae861c457bf33f0d289bedba7daef0
준규 <vpark45@gmail.com>
2022-01-21 16:45:38 +0900

    Solve lesson 12935 using Java Supplier

--- nattybear/level1/제일-작은-수-제거하기/Solution.java
+++ nattybear/level1/제일-작은-수-제거하기/Solution.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 class Solution {
   public int[] solution(int[] arr) {
-    IntStream stream = Arrays.stream(arr);
-    int min = stream.min().getAsInt();
-    int[] answer = stream.filter(x -> x != min).toArray();
+    Supplier<IntStream> supplier = () -> Arrays.stream(arr);
+    int min = supplier.get().min().getAsInt();
+    int[] answer = supplier.get().filter(x -> x != min).toArray();
     if (answer.length == 0)
       return new int[] {-1};
     else
