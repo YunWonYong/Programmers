e56fbbb6144dae76f48cb9f2cf2a7ff698c927cf
준규 <vpark45@gmail.com>
2022-01-21 16:23:36 +0900

    Solve lesson 12934 using Java thanks for Lee-jh01

--- nattybear/level1/정수-제곱근-판별/Solution.java
+++ nattybear/level1/정수-제곱근-판별/Solution.java
@@ -1,9 +1,14 @@
 class Solution {
   public long solution(long n) {
-    long m = Math.sqrt(n);
-    if (m.toString().contains('.'))
+
+    double m = Math.sqrt(n);
+
+    if (m % 1 == 0) {
+      long x = (long) m + 1;
+      return x * x;
+
+    } else {
       return -1;
-    else
-      return Math.pow(m + 1, 2);
+    }
   }
 }
