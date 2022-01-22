8f00067b5b2ef29377a9497a19cbba4f959ca1cb
준규 <vpark45@gmail.com>
2022-01-22 21:30:42 +0900

    Solve lesson using JavaScript

--- nattybear/level1/배열-자르기/solution.js
+++ nattybear/level1/배열-자르기/solution.js
@@ -1,32 +1,25 @@
 function solution(n, left, right) {
-  const gen = go(n)
-  return Array.from(take(right - left + 1, drop(left, gen)))
-}
-
-function* go(n) {
-  for (let i = 1; i <= n; i++) {
-    yield* take(i, repeat(i))
-    yield* drop(i, range(n))
+  const arr = []
+  const remainder = left % n
+  const limit = (right - left + 1) + remainder
+  let cnt = 0
+  for (let i = row(n, left); i <= n; i++) {
+    let j = 1
+    for (; j <= i; j++) {
+      arr.push(i)
+      cnt++
+      if (cnt == limit)
+        return arr.slice(remainder)
+    }
+    for (; j <= n; j++) {
+      arr.push(j)
+      cnt++
+      if (cnt == limit)
+        return arr.slice(remainder)
+    }
   }
 }
 
-function* range(end) {
-  for (let i = 1; i <= end; i++)
-    yield i
-}
-
-function* repeat(n) {
-  while (true)
-    yield n
-}
-
-function* take(n, xs) {
-  for (let i = 0; i < n; i++)
-    yield xs.next().value
-}
-
-function* drop(n, xs) {
-  for (let i = 0; i < n; i++)
-    xs.next()
-  yield* xs
+function row(n, left) {
+  return Math.floor(left / n) + 1
 }
