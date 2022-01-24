d9478eeeec5c426f23a867c8ea933acc5fab4ff6
Lee-jh01 <wjdgmllee0423@naver.com>
2022-01-24 17:19:00 +0900

    소인수분해로 구하기 - 코드 간결하게 하기

--- ljh/java/src/level1/GCDandLCM.java
+++ ljh/java/src/level1/GCDandLCM.java
@@ -7,8 +7,8 @@ public class GCDandLCM {
 	// 소인수분해해서 구하기
 	public int[] solution(int n, int m) {
 		int[] answer = new int[] {1,1};
-		List<Integer> nlist = factorize2(n);
-		List<Integer> mlist = factorize2(m);
+		List<Integer> nlist = factorize(n);
+		List<Integer> mlist = factorize(m);
 		
 		int pointer = 0;
 		int nValue = 0;
@@ -49,29 +49,4 @@ public class GCDandLCM {
 		return list;
 	}
 	
-	// 소인수 분해하기 2(약수가 2인 경우 분리)
-	public List<Integer> factorize2(int n) {
-		List<Integer> list = new ArrayList<Integer>();
-		int divisor = 2;
-		
-		while(divisor == 2) {
-			if(n%divisor == 0) {
-				list.add(divisor);
-				n /= divisor;
-			}else {
-				divisor++;
-			}
-		}
-		
-		while(n!=1) {
-			if(n%divisor == 0) {
-				list.add(divisor);
-				n /= divisor;
-			}else {
-				divisor +=2;
-			}
-		}
-		
-		return list;
-	}
 }
