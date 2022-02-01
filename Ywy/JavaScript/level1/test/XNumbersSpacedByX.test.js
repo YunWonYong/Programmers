0871fcaa1102db15ebc96dc196d91fd5ce8d18c4
ywyi1992 <ywyi1992@gmail.com>
2022-02-02 00:21:35 +0900

    2022-02-02 ywy js add x만큼 간격이 있는 n개의 숫자

---
+++ Ywy/JavaScript/level1/test/XNumbersSpacedByX.test.js
@@ -0,0 +1,13 @@
+import XNumbersSpacedByX from '../XNumbersSpacedByX';
+
+test("2, 5 is [2,4,6,8,10]", () => {
+    expect(new XNumbersSpacedByX().solution(2, 5)).toEqual([2, 4, 6, 8, 10]);
+});
+
+test("4, 3 is [4,8,12]", () => {
+    expect(new XNumbersSpacedByX().solution(4, 3)).toEqual([4, 8, 12]);
+});
+
+test("-4, 2 is [-4, -8]", () => {
+    expect(new XNumbersSpacedByX().solution(-4, 2)).toEqual([-4, -8]);
+});
