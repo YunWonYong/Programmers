ce7cd36cbae4b3f4926395f5f7bb16882e63e665
준규 <vpark45@gmail.com>
2022-02-06 11:58:14 +0900

    Commit before going birthday party

--- nattybear/level1/신고-결과-받기/Solution.java
+++ nattybear/level1/신고-결과-받기/Solution.java
@@ -1,75 +1,38 @@
 import java.util.*;
 
+class Reporters extends HashSet<String> {}
+
+class Graph extends HashMap<String,Reporters> {
+}
+
 class Solution {
   public int[] solution(String[] id_list, String[] report, int k) {
     Graph graph = new Graph();
 
     for (String id : id_list)
-      graph.put(id, new Edge());
+      graph.put(id, new Reporters());
 
-    String[][] pairs = Arrays
-      .stream(report)
-      .map(x -> x.split(" "))
-      .toArray(String[][]::new);
-
-    for (String[] pair : pairs)
-      graph.get(pair[1]).add(pair[0]);
-
+    for (String line : report) {
+      String[] pair = line.split(" ");
+      String id = pair[1];
+      String reporter = pair[0];
+      graph.get(id).add(reporter);
+    }
 
     return new int[] {};
   }
 }
 
-class Edge {
-  private HashSet<String> edge;
+class Counter extends HashMap<String,Integer> {
+  public Counter(String[] id_list) {
+    super();
+    for (String id : id_list)
+      super.put(id, 0);
+  }
 
   public void add(String id) {
-    edge.add(id);
-  }
-}
-
-class Graph {
-  private HashMap<String,Edge> graph;
-
-  public void put(String key, Edge value) {
-    graph.put(key, value);
-  }
-
-  public Edge get(String key) {
-    return graph.get(key);
-  }
-
-  public Pair[] makePairs() {
-    Pair[] pairs = new Pair[graph.size()];
-  }
-
-  public static Pair[] zip(String[] id, Edge edge) {
-  }
-}
-
-class Pair {
-  private String id;
-  private int count;
-
-  public Pair(String id, int count) {
-    this.id = id;
-    this.count = count;
-  }
-
-  public String fst() {
-    return id;
-  }
-
-  public int snd() {
-    return count;
-  }
-}
-
-class Counter {
-  private HashMap<String,Integer> counter;
-
-  public Counter(String[] id_list) {
-    counter = new HashMap<>();
-    Arrays.stream(id_list).map(x -> counter.put(x, 0));
+    int current = super.get(id);
+    int new_current = current + 1;
+    super.put(id, new_current);
   }
 }
