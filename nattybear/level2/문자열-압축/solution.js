96b54246d8db3d67205793878aa3f515668798cc
준규 <vpark45@gmail.com>
2022-02-08 07:43:19 +0900

    Implement compress function

--- nattybear/level2/문자열-압축/solution.js
+++ nattybear/level2/문자열-압축/solution.js
@@ -9,3 +9,29 @@ function drop(n, s) {
     return ""
   return s.slice(n)
 }
+
+function compress(s, n) {
+  let can = []
+  const bag = []
+  function go(s) {
+    if (s.length == 0)
+      return
+    const fst = take(n, s)
+    const tail = drop(n, s)
+    const snd = take(n, tail)
+    if (fst == snd) {
+      can.push(fst)
+      go(tail)
+    }
+    else {
+      can.push(fst)
+      bag.push(can)
+      can = []
+      go(tail)
+    }
+  }
+  go(s)
+  return bag
+}
+
+console.log(compress("hello", 1))
