b3c5092832f8ef1d495e2abd6fac21bc4a22f4a3
ywyi1992 <ywyi1992@gmail.com>
2022-02-07 23:05:03 +0900

    2022-02-07 신고 결과 받기 java modify * refactoring#1

---
+++ Ywy/JAVA/src/level1/test/FebruaryWeekOf2.java
@@ -0,0 +1,13 @@
+package level1.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import level1.test.cases.ReportResultTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ReportResultTest.class })
+public class FebruaryWeekOf2 {
+
+}
