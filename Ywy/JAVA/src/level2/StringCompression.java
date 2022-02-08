eca6afd01fd014ecab05a74edc84c4e25524c906
ywyi1992 <ywyi1992@gmail.com>
2022-02-08 12:48:25 +0900

    2022-02-08 문자열 압축 refactoring end

--- Ywy/JAVA/src/level2/StringCompression.java
+++ Ywy/JAVA/src/level2/StringCompression.java
@@ -1,7 +1,115 @@
 package level2;
 
-public class StringCompression {
+import semantics.Choice;
 
+final class Compression {
+	private final StringBuffer BUFFER;
+	private final String INPUT;
+	private int inputSize;
+	private int compressionSize;
+	private int positionPoint;
+	private int subStrBeginIndex;
+	private int subStrEndIndex;
+	
+	public Compression(String input) {
+		INPUT = input;
+		BUFFER = new StringBuffer();
+	}
+	
+	public Compression setUp() {
+		inputSize = INPUT.length();
+		compressionSize = inputSize;
+		positionPoint = (inputSize / 2);
+		subStrEndIndex = positionPoint;
+		return this;
+	}
+	
+	public int compression() {
+		String matchedStr = getFirstMatchedStr();
+		while (true) {
+			if (matchedStr == null) {
+				compressionSize = bufferFlush().result();
+				if (positionPoint <= 0) {
+					return compressionSize;
+				}
+				matchedStr = bufferClear().getFirstMatchedStr();
+				continue;
+			}
+			matchedStr = matchingSearch(matchedStr);
+		}
+	}
+	
+	private String matchingSearch(String matchedStr) {
+		String nextStr = null;
+		int matchingCount = 1;
+		while (true) {
+			nextStr = getNextStr();
+			if (matchedStr.equals(nextStr)) {
+				matchingCount++;
+				continue;
+			}
+			return unmatched(matchingCount, matchedStr, nextStr);
+		}
+	}
+	
+	private String unmatched(int matchingCount, String matchedStr, String nextStr) {
+		if (matchingCount > 1) {
+			BUFFER.append(matchingCount);
+		}
+		BUFFER.append(matchedStr);
+		return nextStr;
+	}
+	
+	private String getFirstMatchedStr() {
+		subStrEndIndex = positionPoint;
+		return getMatchedStr(0, subStrEndIndex);
+	}
+	
+	private String getNextStr() {
+		return substringPointCheck() ? getMatchedStr(subStrBeginIndex, subStrEndIndex) : null;
+	}
+
+	private boolean substringPointCheck() {
+		subStrBeginIndex = subStrEndIndex;
+		subStrEndIndex += positionPoint;
+		return subStrEndIndex <= inputSize ? true : false;
+	}
+	
+	private String getMatchedStr(int begin, int end) {
+		String subStr = INPUT.substring(begin, end);
+		return isNullOrBlank(subStr) ? null : subStr;
+	}
+	
+	private int result() {
+		int bufferSize = BUFFER.length();
+		--positionPoint;
+		return compressionSize < bufferSize ? compressionSize : bufferSize;
+	}
+	
+	private Compression bufferFlush() {
+		if (subStrEndIndex - positionPoint < inputSize) {
+			BUFFER.append(INPUT.substring(subStrEndIndex - positionPoint, inputSize));
+		}
+		return this;
+	}
+	
+	private Compression bufferClear() {
+		BUFFER.replace(0, inputSize, "");
+		return this;
+	}
+	
+	private boolean isNullOrBlank(String subStr) {
+		return isNull(subStr) || subStr.isEmpty();
+	}
+	
+	private boolean isNull(String subStr) {
+		return subStr == null;
+	}
+	
+}
+
+public class StringCompression {
+	
 	public int solution(String s) {
 		int size = s.length();
 		if (size < 3) {
@@ -64,5 +172,9 @@ public class StringCompression {
 			matchedStr = nextStr;
 		}
 	}
-
+	
+	@Choice
+	public int solution2(String s) {
+		return new Compression(s).setUp().compression();
+	}
 }
