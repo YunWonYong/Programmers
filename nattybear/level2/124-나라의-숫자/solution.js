0cc1e2c9afb25d310334dc0f15514dfae9398b08
준규 <vpark45@gmail.com>
2022-02-13 15:28:50 +0900

    Solve lesson 12899 using JavaScript

--- nattybear/level2/124-나라의-숫자/solution.js
+++ nattybear/level2/124-나라의-숫자/solution.js
@@ -1,30 +1,21 @@
 function solution(n) {
-  if (n == 1)
-    return 1
-  let x = [1]
-  for (let i = 0; i < n-1; i++)
-    x = succ(x)
-  return x.join('')
-}
-
-function succ(xs) {
-  if (xs.length == 0)
-    return [1]
-  const i = xs.length - 1
-  const last = xs[i]
-  if (last == 1) {
-    xs[i] = 2
-    return xs
+  const xs = []
+  while (true) {
+    if (n == 0) {
+      break
+    }
+    if (n < 3) {
+      xs.unshift(n)
+      break
+    }
+    let rem = n % 3
+    let quo = Math.floor(n / 3)
+    if (rem == 0) {
+      quo = quo - 1
+      rem = 3
+    }
+    xs.unshift(rem)
+    n = quo
   }
-  if (last == 2) {
-    xs[i] = 4
-    return xs
-  }
-  const ys = succ(init(xs))
-  ys.push(1)
-  return ys
-}
-
-function init(xs) {
-  return xs.slice(0, xs.length-1)
+  return xs.join('').replace(/3/g, '4')
 }
