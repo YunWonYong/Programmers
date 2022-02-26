33585eda604bdb7d559d81c644ea41d1ecbe7dd1
준규 <vpark45@gmail.com>
2022-02-26 21:39:46 +0900

    Exceed time
    정확성 테스트는 모두 통과했는데 효율성은 모두 시간 초과

--- nattybear/level2/짝지어-제거하기/solution.js
+++ nattybear/level2/짝지어-제거하기/solution.js
@@ -1,32 +1,26 @@
 function solution(s) {
   const xs = s.split('')
-  const ys = remove_pairs(xs)
-  if (ys.length == 0)
+  remove_pair(xs)
+  if (xs.length == 0)
     return 1
   else
     return 0
 }
 
-function remove_pairs(xs) {
-  const ys = remove_pair(xs)
-  if (ys.length == 0)
-    return ys
-  if (ys.length == xs.length)
-    return ys
-  return remove_pairs(ys)
-}
-
 function remove_pair(xs) {
-  if (xs.length < 1)
-    return xs
-  const ys = [...xs]
-  for (let i = 0; i < ys.length-1; i++) {
-    let a = ys[i]
-    let b = ys[i+1]
-    if (a == b) {
-      ys.splice(i, 2)
-      return ys
+  while (true) {
+    const before = xs.length
+    for (let i = 0; i < xs.length-1; i++) {
+      let x = xs[i]
+      let y = xs[i+1]
+      if (x == y) {
+        xs.splice(i, 2)
+        break
+      }
     }
+    const after = xs.length
+    if (before == after)
+      break
   }
-  return ys
+  return xs
 }
