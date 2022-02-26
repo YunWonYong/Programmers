f4b420e5ffadc5fa53269377e0a1f1424e2bcf31
준규 <vpark45@gmail.com>
2022-02-26 22:09:53 +0900

    More exceed time
    길이를 구하는 게 모두 비용일 것 같아서 length를 모두 없앴다
    그런데 오히려 정확성 테스트에서도 시간 초과가 나버렸다
    아무래도 splice가 시간이 오래 걸리는 것 같다

--- nattybear/level2/짝지어-제거하기/solution.js
+++ nattybear/level2/짝지어-제거하기/solution.js
@@ -1,28 +1,25 @@
 function solution(s) {
-  if (s.length % 2 == 1)
-    return 0
   const xs = s.split('')
   remove_pair(xs)
-  if (xs.length == 0)
-    return 1
-  else
+  if (xs[0])
     return 0
+  else
+    return 1
 }
 
 function remove_pair(xs) {
+  let i = 0
   while (true) {
-    const before = xs.length
-    for (let i = 0; i < xs.length-1; i++) {
-      let x = xs[i]
-      let y = xs[i+1]
-      if (x == y) {
-        xs.splice(i, 2)
-        break
-      }
-    }
-    const after = xs.length
-    if (before == after)
+    let x = xs[i]
+    let y = xs[i+1]
+    if (!x || !y)
       break
+    if (x == y) {
+      xs.splice(i, 2)
+      i = 0
+      continue
+    }
+    i++
   }
   return xs
 }
