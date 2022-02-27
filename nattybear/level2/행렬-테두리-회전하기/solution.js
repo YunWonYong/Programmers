7bce7073b5ada7c670af4525c759649a7792ae0e
준규 <vpark45@gmail.com>
2022-02-28 08:51:29 +0900

    Add move methods to Point class and add next function

--- nattybear/level2/행렬-테두리-회전하기/solution.js
+++ nattybear/level2/행렬-테두리-회전하기/solution.js
@@ -3,6 +3,22 @@ class Point {
     this.x = x
     this.y = y
   }
+
+  up() {
+    return new Point(this.x - 1, this.y)
+  }
+
+  down() {
+    return new Point(this.x + 1, this.y)
+  }
+
+  left() {
+    return new Point(this.x, this.y - 1)
+  }
+
+  right() {
+    return new Point(this.x, this.y + 1)
+  }
 }
 
 function make_matrix(row, column) {
@@ -18,4 +34,32 @@ function make_matrix(row, column) {
     line = []
   }
   return lines
+}
+
+function next(point, query) {
+  let [x1, y1, x2, y2] = query
+  if (point.x == x1) {
+    if (point.y == y2) {
+      return point.down()
+    }
+    return point.right()
+  }
+  if (point.y == y2) {
+    if (point.x == x2) {
+      return point.left()
+    }
+    return point.down()
+  }
+  if (point.x == x2) {
+    if (point.y == y1) {
+      return point.up()
+    }
+    return point.left()
+  }
+  if (point.y == y1) {
+    if (point.x == x1) {
+      return right()
+    }
+    return up()
+  }
 }
