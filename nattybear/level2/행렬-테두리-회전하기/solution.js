eb3be4300d8da41d91ba54054b1b6b5ae585b77d
준규 <vpark45@gmail.com>
2022-02-28 12:11:28 +0900

    Implement Matrix class

--- nattybear/level2/행렬-테두리-회전하기/solution.js
+++ nattybear/level2/행렬-테두리-회전하기/solution.js
@@ -19,47 +19,63 @@ class Point {
   right() {
     return new Point(this.x, this.y + 1)
   }
-}
 
-function make_matrix(row, column) {
-  let x = 1
-  let line = []
-  const lines = []
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < column; j++) {
-      line.push(x)
-      x++
+  next(point, query) {
+    const [x1, y1, x2, y2] = query
+    if (point.x == x1) {
+      if (point.y == y2) {
+        return point.down()
+      }
+      return point.right()
     }
-    lines.push(line)
-    line = []
-  }
-  return lines
-}
-
-function next(point, query) {
-  let [x1, y1, x2, y2] = query
-  if (point.x == x1) {
     if (point.y == y2) {
+      if (point.x == x2) {
+        return point.left()
+      }
       return point.down()
     }
-    return point.right()
-  }
-  if (point.y == y2) {
     if (point.x == x2) {
+      if (point.y == y1) {
+        return point.up()
+      }
       return point.left()
     }
-    return point.down()
-  }
-  if (point.x == x2) {
     if (point.y == y1) {
+      if (point.x == x1) {
+        return point.right()
+      }
       return point.up()
     }
-    return point.left()
-  }
-  if (point.y == y1) {
-    if (point.x == x1) {
-      return right()
-    }
-    return up()
   }
 }
+
+class Matrix {
+  constructor(row, column) {
+    let x = 1
+    let line = []
+    this.matrix = []
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < column; j++) {
+        line.push(x)
+        x++
+      }
+      this.matrix.push(line)
+      line = []
+    }
+  }
+
+  get(point) {
+    return this.matrix[point.x-1][point.y-1]
+  }
+
+  set(point, value) {
+    this.matrix[point.x-1][point.y-1] = value
+  }
+}
+
+function solution(rows, columns, queries) {
+}
+
+const m = new Matrix(2,2)
+m.set(new Point(1,1), 7)
+console.log(m.matrix)
